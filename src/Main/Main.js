0980662b81b2e4280f9d328ddf4125a0e390a14a
Gonpeche <espeche.g@gmail.com>
2019-05-20 10:07:08 -0300

    first commit

---
+++ src/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Chingu Tier 2 Challenge - Connect 4
+      </header>
+    </div>
+  );
+}
+
+export default Main;
